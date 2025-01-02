13ad3e81e52c26b4120e7c835e0a6e00df72b687
barmolay4ik <164476255+barmolay4ik@users.noreply.github.com>
2025-01-02 14:06:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def venom(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         hours, remainder = divmod(remaining_time.seconds, 3600)
         minutes, _ = divmod(remainder, 60)
         await update.message.reply_text(
-            f"@{username}, НЕ АХУЕВАЙ\nНапишешь через {hours}ч {minutes}м, петух!\nНе думай что я слабак"
+            f"@{username}, НЕ АХУЕВАЙ\nНапишешь через {hours}ч {minutes}м, петух!\nНе думай что я слабак\n\nТремор походу привязался к ней"
         )
         return
 
